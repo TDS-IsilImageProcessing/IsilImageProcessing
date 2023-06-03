73b814a1e5177d56c4fc71e53e408ad0efd5ddbc
YunusGoven <yunuss.gvn@gmail.com>
2023-06-03 22:19:52 +0200

    javadoc + gui

--- src/ImageProcessing/Contours/ContoursLineaire.java
+++ src/ImageProcessing/Contours/ContoursLineaire.java
@@ -7,6 +7,15 @@ import ImageProcessing.Lineaire.FiltrageLineaireLocal;
  */
 public class ContoursLineaire {
 
+
+    /**
+     * Parcout l'image et effectue un masque de convolution avec le filtre selon la direction.
+     * Technique utilisé pour detecter les contours. La direction permet de mettre en évidence les variations d'intensité dans les directions spécifiques
+     * Resultat : les pixels sur les contours ou les bord de l'images ont des valeurs élevées dans la direction du gradient, les régions lisses ont des valeurs faible
+     * @param image
+     * @param dir
+     * @return
+     */
     public static int[][] gradientPrewitt(int[][] image,int dir) {
         double[][] masque = dir == 1 ? new double[][] {
                 {1,0,-1},
@@ -23,6 +32,15 @@ public class ContoursLineaire {
         return resultat;
     }
 
+    /**
+     * Parcout l'image et effectue un masque de convolution avec le filtre selon la direction.
+     * meme chose que le prewitt mais le masque est différents
+     * plus sensible pour les contours, meilleur précision des contours
+     * Resultat : les contours et les transition d'intensité sont mis en évidence
+     * @param image
+     * @param dir
+     * @return
+     */
     public static int[][] gradientSobel(int[][] image,int dir) {
         double[][] masque = dir == 1 ? new double[][] {
                 {1,0,-1},
@@ -38,6 +56,17 @@ public class ContoursLineaire {
 
         return resultat;
     }
+
+    /**
+     * Parcout l'image et effectue un masque de convolution avec le filtre selon la direction.
+     * Detecte les régions de l'image ou l'intensité varie rapidement (contours, bord, detail)
+     * Met en evidence les transitions d'intensite rapide et les regions ou l'intensité change brusquement
+     * Permet de detecter les contours et bords precis
+     * a utiliser avec d'autres techniques de filtrage ou seuillage
+     *
+     * @param image
+     * @return
+     */
     public static int[][] laplacien4(int[][] image) {
         double[][] masque = new double[][] {
                 {0,1,0},
@@ -50,6 +79,15 @@ public class ContoursLineaire {
         return resultat;
     }
 
+    /**
+     * Parcout l'image et effectue un masque de convolution avec le filtre selon la direction.
+     * meme chose que le 4 mais prend en compte les voisins diagonaux et plus des voisions horizontaux et verticaux
+     * plus sensible au detail et au coutours diagonaux
+     * a utiliser avec d'autres techniques de filtrage ou seuillage
+     *
+     * @param image
+     * @return
+     */
     public static int[][] laplacien8(int[][] image) {
         double[][] masque = new double[][] {
                 {1,1,1},
