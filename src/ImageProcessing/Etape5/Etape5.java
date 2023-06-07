61023315f9a8411683a79853502ff5bd0777af04
YunusGoven <yunuss.gvn@gmail.com>
2023-06-07 15:11:16 +0200

    re

---
+++ src/ImageProcessing/Etape5/Etape5.java
@@ -0,0 +1,11 @@
+package ImageProcessing.Etape5;
+
+public class Etape5 {
+
+    public static int[][] etape1(int[][] image) {
+        //filtre passe bas 70
+        // filtre median 3
+
+        return image;
+    }
+}
